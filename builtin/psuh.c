7b84af859fef53748cad9624b4f6826601a2b563
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2022-10-01 13:29:17 -0300

    psuh: show parameters & config opts
    Demonstrate how a contributor can use the context provided to them when
    a command is invoked from git.c. This change causes psuh to dump all
    arguments provided by the user, as well as print the relative directory
    from the prefix. It also demonstrates how to check the configuration
    files by printing the user's configured full name.
    
    Signed-off-by: Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>

--- builtin/psuh.c
+++ builtin/psuh.c
@@ -1,7 +1,27 @@
 #include "builtin.h"
+#include "config.h"
 
 int cmd_psuh(int argc, const char **argv, const char *prefix)
 {
+	int i;
+	const char *cfg_name;
+
+	printf(Q_("Your args (there is %d):\n",
+		  "Your args (there are %d):\n",
+		  argc),
+	       argc);
+	for (i = 0; i < argc; i++)
+		printf("%d: %s\n", i, argv[i]);
+
+	printf(_("Your current working directory:\n<top-level>%s%s\n"),
+	      prefix ? "/" : "", prefix ? prefix : "");
+
+	git_config(git_default_config, NULL);
+	if (git_config_get_string_tmp("user.name", &cfg_name) > 0)
+		printf(_("No name is found in config\n"));
+	else
+		printf(_("Your name: %s\n"), cfg_name);
+
 	printf(_("Pony saying hello goes here.\n"));
 	return 0;
 }
