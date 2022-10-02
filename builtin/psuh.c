d39cab6b3bede48c8d3442873c298cbdc69356ee
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2022-10-01 23:49:32 -0300

    psuh: print the current branch
    Demonstrate how to use wt-status to find information about the current
    working tree's status.
    
    The working tree status can reveal lots of interesting things about the
    current context, for example, which branch the user is on, whether
    changes are staged or not, and on which files. This can be extremely
    handy to inform yourself of what the user is working on or likely to
    care about. Luckily, cmd_status in builtin/commit.c is easy to read and
    a good demonstration of how to use wt-status.h.
    
    Signed-off-by: Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>

--- builtin/psuh.c
+++ builtin/psuh.c
@@ -1,10 +1,12 @@
 #include "builtin.h"
 #include "config.h"
+#include "wt-status.h"
 
 int cmd_psuh(int argc, const char **argv, const char *prefix)
 {
 	int i;
 	const char *cfg_name;
+	struct wt_status status;
 
 	printf(Q_("Your args (there is %d):\n",
 		  "Your args (there are %d):\n",
@@ -16,11 +18,14 @@ int cmd_psuh(int argc, const char **argv, const char *prefix)
 	printf(_("Your current working directory:\n<top-level>%s%s\n"),
 	      prefix ? "/" : "", prefix ? prefix : "");
 
+	wt_status_prepare(the_repository, &status);
 	git_config(git_default_config, NULL);
 	if (git_config_get_string_tmp("user.name", &cfg_name) > 0)
 		printf(_("No name is found in config\n"));
 	else
 		printf(_("Your name: %s\n"), cfg_name);
+
+	printf(_("Your current branch: %s\n"), status.branch);
 
 	printf(_("Pony saying hello goes here.\n"));
 	return 0;
